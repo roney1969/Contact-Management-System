c51ccb3d5616b07e4d0c03787068cf2c95da23ce
Roney George <roney1969@gmail.com>
2023-09-15 15:14:14 +0100

    changes added

--- src/main/java/WorkContactCategory.java
+++ src/main/java/WorkContactCategory.java
@@ -1,21 +1,33 @@
-public class WorkContactCategory implements IContactCategory{
-    @Override
-    public void addContact() {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+public class WorkContactCategory implements IContactCategory{
+    ArrayList<Contact> workContactList = new ArrayList<Contact>;
+    HashMap<String, Contact> workContactMap = new HashMap<String, Contact>();
+
+    @Override
+    public void addContact(String name, String email, int phoneNumber) {
+        Contact contact = new Contact(name, email, phoneNumber);
+        workContactList.add(contact);
+        workContactMap.put(name, contact);
     }
 
     @Override
-    public void removeContact() {
-
+    public void removeContact(String name) {
+        Contact contact = workContactMap.get(name);
+        workContactList.remove(contact);
     }
 
     @Override
     public void viewContacts() {
-
+        for (Contact myContact : workContactList) {
+            System.out.println(myContact.getName() + ", " + myContact.getEmail() + ", " + myContact.getPhoneNumber());
+        }
     }
 
     @Override
-    public void searchContacts() {
-
+    public void searchContacts(String name) {
+        Contact myContact = workContactMap.get(name);
+        System.out.println(myContact.getName() + ", " + myContact.getEmail() + ", " + myContact.getPhoneNumber());
     }
 }
