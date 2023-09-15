90ed7fac77a82b6b3be86017baad79b7ebcb1895
John <jmmac2000@gmail.com>
2023-09-15 15:11:23 +0100

    added ContactManager class

---
+++ src/main/java/ContactManager.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class ContactManager {
+    HashMap<ContactCategoryName, IContactCategory> contactCategoryMap = new HashMap<>();
+    IContactCategory personalContactCategory = new PersonalContactCategory();
+    IContactCategory workContactCategory = new WorkContactCategory();
+    public ContactManager() {
+        contactCategoryMap.put(ContactCategoryName.PERSONAL, personalContactCategory);
+        contactCategoryMap.put(ContactCategoryName.WORK, workContactCategory);
+    }
+    public void addContact(ContactCategoryName contactCategoryName, String name, String email, int phoneNumber) {
+        contactCategoryMap.get(contactCategoryName).addContact(name, email, phoneNumber);
+    }
+    public void removeContact(ContactCategoryName contactCategoryName, String name) {
+        contactCategoryMap.get(contactCategoryName).removeContact(name);
+    }
+    public void viewContacts(ContactCategoryName contactCategoryName) {
+        contactCategoryMap.get(contactCategoryName).viewContacts();
+    }
+    public void searchContacts(ContactCategoryName contactCategoryName, String name) {
+        contactCategoryMap.get(contactCategoryName).searchContacts(name);
+    }
+}
